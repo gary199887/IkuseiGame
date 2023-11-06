c2900a34e64a80c902c2416c2198ff94aa7adcd2
gary199887 <47510884+gary199887@users.noreply.github.com>
2023-11-06 16:17:44 +0900

    ボタンクリック時メソッドを書くところとしてButtonFuctions.cs追加、ステータス間に改行を挿入

---
+++ Assets/Scripts/GameScene/ButtonFuctions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFuctions : MonoBehaviour
+{
+    public void onWateringButtonClicked() {     // 例として水やりボタンがクリックされた時呼び出す空メソッド
+        Debug.Log("水やりを選択した");
+    }
+
+    public void onItemButtonClicked() {        //　アイテム使用ボタンがクリックされた時呼び出すメソッド
+    
+    }
+}
