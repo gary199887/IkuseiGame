e2b18438bb78e8197aca2f9a22660972b5984fa0
gary199887 <47510884+gary199887@users.noreply.github.com>
2023-11-06 14:39:24 +0900

    PlayerController追加、キャラクラス（仮）追加

---
+++ Assets/Scripts/GameScene/classForData/Chara.cs
@@ -0,0 +1,11 @@
+public class Chara
+{
+    // プレイヤに開示する情報 下限0, 上限まだ決めてない
+    int hp;             // HP
+    int power;          // 筋力
+    int intelligent;    // 知力
+    int mental;         // メンタル
+
+    // 開示しない情報
+    int friendly;       // 好感度（初期値0, 上限、下限(マイナス)を決める）
+}
