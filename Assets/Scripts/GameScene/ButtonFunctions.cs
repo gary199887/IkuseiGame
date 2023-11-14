e7cc137d227005d24b83e01d3de33f2985589b08
gary199887 <47510884+gary199887@users.noreply.github.com>
2023-11-14 10:50:12 +0900

    行動レベルや成功率の調整
    ・行動別の成功率を追加（行動レベルによる影響）
    ・行動別のレベルや成功率をUI上に表示
    ・命名ButtonFuctions->ButtonFunctionsスペルミスを直し

---
+++ Assets/Scripts/GameScene/ButtonFunctions.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    [SerializeField] DialogManager dialogManager;   // ダイアログ表示用のマネージャーObj
+    [SerializeField] GameObject buttons;            // ボタン全体、ボタンon/off切り替え用GameObj
+    [SerializeField] GameDirector gameDirector;     // パラメーター変更用GameDirector Obj
+    [SerializeField] ButtonHintManager buttonHintManager;
+    public Effect effect;                           // 行動に起こされた変化
+    enum actionWithLv { 投げつける, 勉強させる, 話しかける };      // 行動レベルのある行動名(失敗する可能性がある)
+    Action[] actions = {new Action("Throw"), new Action("Study"), new Action("Talk")};  // 行動Lv計算用(投げつけ、勉強、話しかける)
+    int doLvUp = -1;
+
+    public void onWateringButtonClicked() {     // 水やりボタンがクリックされた時呼び出すメソッド
+        closeButtons();
+        string[] msg;
+       
+        effect = new Effect(3, 30, 0, 0, 0, 1);
+        GameDirector.changeParameter(effect);
+        msg = new string[] { "水やりをした", effect.getPlusMsg(), effect.getMinusMsg()};
+       
+        dialogManager.showDialog(msg);
+    }
+
+
+    public void onThrowingButtonClicked() {     // 投げつけるボタンクリックされた時に呼び出すメソッド
+        doActionWithLv(actionWithLv.投げつける);
+    }
+
+    public void onStudyButtonClicked(){        // 勉強させるボタンクリックされた時に呼び出すメソッド
+        doActionWithLv(actionWithLv.勉強させる);
+    }
+    public void onItemButtonClicked() {        //　アイテム使用ボタンがクリックされた時呼び出すメソッド
+        string[] msg = { "アイテムを使用した" };
+        dialogManager.showDialog(msg);
+    }
+
+    public void onTalkingButtonClicked()    //　会話ボタンがクリックされた時呼び出すメソッド
+    {
+        doActionWithLv(actionWithLv.話しかける);
+    }
+
+    public void onOutingButtonClicked()     //　会話ボタンがクリックされた時呼び出すメソッド
+    {
+        // イベント発生処理追加予定
+        string[] msg = { "お出かけした" };
+        dialogManager.showDialog(msg);
+    }
+
+    // 行動選択ボタンを消す
+    public void closeButtons() { 
+        buttons.SetActive(false);
+    }
+
+    // 行動選択ボタンを戻す
+    public void showButtons() {
+        buttons.SetActive(true);
+    }
+
+    // 行動選択後のダイアログ閉じる時に呼び出すメソッド
+    public void afterActionDialogClosed() {
+        gameDirector.revealStatusInUI();
+
+        if (doLvUp > -1) {  // レベルアップしたかどうか
+            // アップした行動レベルをUI上に反映
+            buttonHintManager.lvUpInUI(doLvUp, actions[doLvUp].getLv());
+            // 判断用の変数を-1に戻す
+            doLvUp = -1;
+        }
+
+        // 成功率をUIに反映
+        buttonHintManager.successChangeInUI(GameDirector.chara.getHp(), actions);
+
+        // 時間加算  日数経過がある場合はtrue 
+        if (!gameDirector.addTime(effect.time))     // 日数が経過しなかった場合（or エンディングに進む場合）
+            effect = null;                          // effectをnullに戻す
+
+        // 時間をUIに反映
+        gameDirector.revealTimeUI();
+    }
+
+    // 抽選（パーセンテージ）メソッド
+    bool getSuccessOrNot(int percentage) {
+        if(Random.Range(1, 101) <= percentage)
+            return true;
+        return false;
+    }
+
+    // 失敗することがある行動をとる時に呼び出すメソッド
+    void doActionWithLv(actionWithLv actionName) {
+        string[] msg;           // ダイアログに表示されるメッセージ
+        string actionMsg = "";  // 行動別に変わる最初のメッセージ
+        if (getSuccessOrNot(GameDirector.chara.getHp() + 20 + actions[(int)actionName].getLv() * 5))   // キャラのHPに応じて行動成功/失敗判定　20（基礎成功率）+ キャラHP (0 ~ 100)
+        {
+            // 行動lvに応じて変化する値(power)が変動  5（基礎値） + 30 x 行動lv(1 ~ 5) / 5
+            effect = new Effect();
+            effect.time = 3;
+            effect.hp = -30;
+            int changeValue = (int)Mathf.Ceil(30 * ((float)actions[(int)actionName].getLv() / 5));
+            switch (actionName) {
+                case actionWithLv.投げつける:
+                    effect.power = changeValue;
+                    actionMsg = "投げつけた";
+                    break;
+                case actionWithLv.勉強させる:
+                    effect.intelligent = changeValue;
+                    actionMsg = "勉強させた";
+                    break;
+                case actionWithLv.話しかける:
+                    effect.mental = changeValue;
+                    actionMsg = "話しかけた";
+                    break;
+            }
+
+
+            // 行動回数追加、レベルアップチェック
+            string lvUpMsg = actions[(int)actionName].doAction();
+            // エフェクトのパラメータに対応したキャラステータス変更処理
+            GameDirector.changeParameter(effect);            
+
+            // 表示メッセージ（レベルアップしたか）
+            if (lvUpMsg != null) { msg = new string[] { actionMsg, effect.getPlusMsg(), effect.getMinusMsg(), lvUpMsg }; doLvUp = (int)actionWithLv.投げつける; }
+            else msg = new string[] { actionMsg, effect.getPlusMsg(), effect.getMinusMsg() };
+        }
+        else    // 行動失敗処理
+        {
+            // 時間経過。体力、好感度減算
+            effect = new Effect(3, -20, 0, 0, 0, -2);
+            // エフェクトのパラメータに対応したキャラステータス変更処理
+            GameDirector.changeParameter(effect);
+            actionMsg = "行動に失敗した";
+            // 表示メッセージ
+            msg = new string[] { actionMsg, effect.getPlusMsg(), effect.getMinusMsg() };
+        }
+        // メッセージをダイアログに表示
+        dialogManager.showDialog(msg);
+    }
+}
