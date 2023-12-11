39676b2807fc729ab3c662749a8c9bab3ebdc902
gary199887 <47510884+gary199887@users.noreply.github.com>
2023-12-11 15:47:30 +0900

    不必要なimportを削除

--- Assets/Scripts/GameScene/ActionSelector.cs
+++ Assets/Scripts/GameScene/ActionSelector.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ActionSelector : MonoBehaviour
