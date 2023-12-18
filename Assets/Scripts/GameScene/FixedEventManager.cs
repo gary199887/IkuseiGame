75ff51877504a8db9a3d30dbfe83398182a01bba
gary199887 <47510884+gary199887@users.noreply.github.com>
2023-12-18 16:15:23 +0900

    固定イベントにXルート追加

--- Assets/Scripts/GameScene/FixedEventManager.cs
+++ Assets/Scripts/GameScene/FixedEventManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class FixedEventManager : MonoBehaviour
@@ -10,6 +11,7 @@ public class FixedEventManager : MonoBehaviour
     [SerializeField] Sprite[] sprites;                  // 画像データ（データ指定用）
     [SerializeField] SpriteRenderer eventImg;           // 画像データ代入用
     public static int route;                            // ルート（初期値-1、一回目の固定イベントでルート決定(0 ~ 2)）
+    int thisTime;
     void Start()
     {
         route = -1;
@@ -22,7 +24,7 @@ public class FixedEventManager : MonoBehaviour
     public void occurFixedEvent(int currentDay)
     {
         // 何週目の固定イベントを計算
-        int thisTime = currentDay / 7;
+        thisTime = currentDay / 7;
         Chara chara = new Chara(GameDirector.chara);
         // 7日目から8日目に進む時に発生
         if (thisTime == 1)
@@ -50,8 +52,8 @@ public class FixedEventManager : MonoBehaviour
             }
             else
             { // all equal (現段階ではひとまず筋力ルートに進む)
-                doFixedEvent(fixedEventList.fixedEvents[0]);
-                route = 0;
+                doFixedEvent(fixedEventList.fixedEvents[15]);
+                route = 3;
             }
         }
         else if (thisTime == 2)
@@ -77,15 +79,41 @@ public class FixedEventManager : MonoBehaviour
                     else
                         doFixedEvent(fixedEventList.fixedEvents[8]);
                     break;
+                case 3:     // 均等ルート
+                    if (chara.getPower() == chara.getIntelligent() && chara.getPower() == chara.getMental())    // 均等
+                        doFixedEvent(fixedEventList.fixedEvents[15]);
+                    else　　　// 数値がそろってない
+                    {
+                        doFixedEvent(fixedEventList.fixedEvents[16]);
+                        route = 4;
+                    }
+                    break;
             }
         }
         else
-        { // 三週目の固定イベント未実装
+        { // 三週目の固定イベント
             int friendlyUpLine = 50;
-            if (chara.getFriendly() >= friendlyUpLine)
-                doFixedEvent(fixedEventList.fixedEvents[9 + route * 2]);
+            if (route < 3)
+                if (chara.getFriendly() >= friendlyUpLine)
+                    doFixedEvent(fixedEventList.fixedEvents[9 + route * 2]);
+                else
+                    doFixedEvent(fixedEventList.fixedEvents[10 + route * 2]);
             else
-                doFixedEvent(fixedEventList.fixedEvents[10 + route * 2]);
+            {
+                if (route == 3)
+                {
+                    if (chara.getPower() == chara.getIntelligent() && chara.getPower() == chara.getMental())
+                        doFixedEvent(fixedEventList.fixedEvents[17]);
+                }
+                else
+                {
+                    if (chara.getPower() == chara.getIntelligent() && chara.getPower() == chara.getMental())
+                        doFixedEvent(fixedEventList.fixedEvents[15]);
+                    else
+                        doFixedEvent(fixedEventList.fixedEvents[16]);
+                }
+            }
+
         }
     }
 
@@ -93,8 +121,31 @@ public class FixedEventManager : MonoBehaviour
     {
         fixedEvent.happened = true;
         dialogManager.showDialog(CommonFunctions.stringAddToLast(fixedEvent.msg, "4時間休憩した..."));    // 固定イベントメッセージ表示(最後に4時間休憩したと文言追加)
+        if (fixedEvent.id == 16)
+        {
+            Chara chara = new Chara(GameDirector.chara);
+            int statusAverage = (chara.getPower() + chara.getIntelligent() + chara.getMental()) / 3;
+            fixedEvent.effect = Effect.statusToTargetChara(chara, new Chara(0, statusAverage, statusAverage, statusAverage, 0));
+        }
         actionSelector.effect = fixedEvent.effect.plusEffect(new Effect(4, 20));    // イベントの影響に日付経過の影響を追加（4時間経過、HP＋20）
-        showEventImg(sprites[fixedEvent.id]);
+        if (fixedEvent.id < 15)
+            showEventImg(sprites[fixedEvent.id]);
+        else
+        {
+            switch (thisTime)
+            {
+                case 1:
+                    showEventImg(sprites[2]);
+                    break;
+                case 2:
+                    showEventImg(sprites[8]);
+                    break;
+                case 3:
+                    showEventImg(sprites[14]);
+                    break;
+            }
+        }
+
     }
 
     public void showEventImg(Sprite img)
