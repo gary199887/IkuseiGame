96270b93c0ea6849bfa2d166237f415f49953e8e
gary199887 <47510884+gary199887@users.noreply.github.com>
2024-01-22 14:50:09 +0900

    セーブデータがない場合ロードボタン非表示

--- Assets/Scripts/GameScene/Saves/LoadButton.cs
+++ Assets/Scripts/GameScene/Saves/LoadButton.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class LoadButton :CommonFunctions, MyButton
 {
+    void Start()
+    {
+        SaveData saveData = SaveIO.DataLoad();
+        if(saveData == null) gameObject.SetActive(false);
+    }
     public void loadButtonClicked() {
         GameDirector.loadGame = true;
         SceneManager.LoadScene("GameScene");
